8e789e04fc89409f46f16c119eadd73324cfe4bc
Gayu EJ <41654397+GayuEJ@users.noreply.github.com>
2018-08-10 23:31:12 -0500

    Updated port Number

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 // Updated todoStatus for a user
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/sampledb');
+mongoose.connect('mongodb://127.0.0.1:27017/sampledb');
 
 var Schema = mongoose.Schema;
 
